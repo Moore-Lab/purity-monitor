5d1c7c1db53607b9deebeb205845c61c89a8f295
yalenexo <yale.nexo@gmail.com>
2022-06-28 20:25:17 -0400

    update instrument functions and wrappers

--- devices/pitaya.py
+++ devices/pitaya.py
@@ -68,9 +68,10 @@ class RedPitaya():
         self.inst.tx_txt('ACQ:TRIG:LEV {} mV'.format(level))
         self.inst.tx_txt('ACQ:TRIG:DLY {}'.format(delay))
 
-    def ext_trigger(self, edge='P'):
+    def ext_trigger(self, edge='P', delay=0):
         ## setup an external trigger
         self.inst.tx_txt('ACQ:TRIG EXT_{}E'.format(edge))
+        self.inst.tx_txt('ACQ:TRIG:DLY {}'.format(int(delay)))
 
     def start(self):
         self.inst.tx_txt('ACQ:START')
